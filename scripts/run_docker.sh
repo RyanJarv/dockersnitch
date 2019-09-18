49f0ff3902d63d7bb6302b86dc9df5d6feda2802
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2019-09-17 17:34:00 -0700

    Show short help after running scripts/run_docker.sh

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run -it -v $(pwd):/go/src/github.com/RyanJarv/dockersnitch -p 127.0.0.1:33504:33504 -w /go/src/github.com/RyanJarv/dockersnitch --privileged --pid host dockersnitch sh -c "go run main.go & bash -l"
+docker run -it -v $(pwd):/go/src/github.com/RyanJarv/dockersnitch -p 127.0.0.1:33504:33504 -w /go/src/github.com/RyanJarv/dockersnitch --privileged --pid host dockersnitch \
+sh -c "go run main.go & echo 'On the host machine use \"nc localhost 33504\" to white/black list connections (w/b <enter>)';  bash -l"
